f60f0d7c827fad46f08d11542b1d54d46dadaf8b
CumiTerbang <gitarringsek@gmail.com>
2021-07-17 20:21:18 +0700

    update Splash Screen

--- app/src/main/java/com/haryop/haryomusicplayer/ui/MainActivity.kt
+++ app/src/main/java/com/haryop/haryomusicplayer/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.haryop.haryomusicplayer
+package com.haryop.haryomusicplayer.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.haryop.haryomusicplayer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/haryop/haryomusicplayer/utils/ConstantsObj.kt
@@ -0,0 +1,5 @@
+package com.haryop.mynewsportal.utils
+
+object ConstantsObj {
+    const val API_BASEURL = "https://itunes.apple.com/"
+}
